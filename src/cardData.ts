2fb58cf6f9b0ddcfd9605f47cb05df0440dbf815
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-08 22:40:24 -0300

    created the card buttons and the hover effect

--- src/cardData.ts
+++ src/cardData.ts
@@ -13,7 +13,8 @@ export interface CardData {
 export const cards: CardData[] = [
   {
     id: 1,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 1",
     description:
       "Descrição do Card 1. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -27,7 +28,8 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -41,7 +43,23 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Título do Card 2",
+    description:
+      "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
+    techStack: [
+      { name: "React" },
+      { name: "HTML" },
+      { name: "Sass" },
+      { name: "WebStorm" },
+      { name: "GitLab" },
+    ],
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -55,7 +73,8 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -69,7 +88,38 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Título do Card 2",
+    description:
+      "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
+    techStack: [
+      { name: "Node.js" },
+      { name: "HTML" },
+      { name: "Sass" },
+      { name: "WebStorm" },
+      { name: "GitLab" },
+    ],
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Título do Card 2",
+    description:
+      "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
+    techStack: [
+      { name: "TypeScript" },
+      { name: "HTML" },
+      { name: "Sass" },
+      { name: "WebStorm" },
+      { name: "GitLab" },
+    ],
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -83,7 +133,8 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -97,7 +148,8 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -111,7 +163,8 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
@@ -125,7 +178,23 @@ export const cards: CardData[] = [
   },
   {
     id: 2,
-    image: "https://via.placeholder.com/300x200",
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Título do Card 2",
+    description:
+      "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
+    techStack: [
+      { name: "JavaScript" },
+      { name: "HTML" },
+      { name: "Sass" },
+      { name: "WebStorm" },
+      { name: "GitLab" },
+    ],
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Título do Card 2",
     description:
       "Descrição do Card 2. Esta é uma breve descrição do card para demonstrar o layout.",
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Card.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faVideo } from "@fortawesome/free-solid-svg-icons";
+
 interface TechStackItem {
   name: string;
 }
@@ -21,8 +24,8 @@ const Card: React.FC<CardProps> = ({
   return (
     <div className="card">
       <img src={image} alt={title} />
-      <div className="content">
-        <h2 className="title">{title}</h2>
+      <div className="card-details">
+        <h2>{title}</h2>
         <div className="tech-stack">
           {techStack.map((tech, index) => (
             <div key={index} className="tech-stack-item">
@@ -30,13 +33,16 @@ const Card: React.FC<CardProps> = ({
             </div>
           ))}
         </div>
-      </div>
-      <div className="card-details">
         <p>{description}</p>
       </div>
+
       <div className="buttons">
-        <button className="btn">Botão 1</button>
-        <button className="btn">Botão 2</button>
+        <button className="btn">
+          <FontAwesomeIcon icon={faCode} />
+        </button>
+        <button className="btn">
+          <FontAwesomeIcon icon={faVideo} />
+        </button>
       </div>
     </div>
   );
