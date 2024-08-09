267f8c143b70f62a3ed575ae2b0efa2d961e268f
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-09 20:45:28 -0300

    new design of cards

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -26,45 +26,20 @@ const Card: React.FC<CardProps> = ({
   codeLink,
   videoLink,
 }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
-
   return (
-    <div className={`card ${showDetails ? "show-details" : ""}`}>
-      <img src={image} alt={title} />
-      <div className="card-details">
-        <h2>{title}</h2>
-        <div className="tech-stack">
-          {techStack.map((tech, index) => (
-            <div key={index} className="tech-stack-item">
-              {tech.name}
-            </div>
-          ))}
-        </div>
-        <p>{description}</p>
-      </div>
-
-      <div className="buttons">
-        <div className="info-button">
-          <button className="btn info-btn" onClick={toggleDetails}>
-            <FontAwesomeIcon icon={faInfo} />
-          </button>
-        </div>
-        <div className="code-video-btt">
-          {codeLink && (
-            <Link to={codeLink} target="_blank" className="btn">
-              <FontAwesomeIcon icon={faCode} />
-            </Link>
-          )}
-          {videoLink && (
-            <Link to={videoLink} target="_blank" className="btn">
-              <FontAwesomeIcon icon={faVideo} />
-            </Link>
-          )}
-        </div>
+    <div className="card">
+      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+      </svg>
+      <div className="card__content">
+        <p className="card__title">Project Name</p>
+        <p className="card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco.
+        </p>
+        <button className="card__button">Video</button>
+        <button className="card__button secondary">Source Code</button>
       </div>
     </div>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -14,18 +14,11 @@ const Navbar: React.FC<NavbarProps> = ({
   onTechChange,
   techOptions,
 }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <nav className="navbar">
-      {/* Navbar tradicional para telas maiores */}
-      <div className="navbar-menu desktop">
+      <div className="navbar-menu desktop radio-inputs">
         {techOptions.map((tech, index) => (
-          <label key={index} className="navbar-item">
+          <label key={index} className="navbar-item radio">
             <input
               type="radio"
               name="tech-stack"
@@ -33,31 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({
               checked={selectedTech === tech}
               onChange={() => onTechChange(tech)}
             />
-            {tech}
-          </label>
-        ))}
-      </div>
-
-      {/* Ícone de Hambúrguer para telas menores */}
-      <div className="hamburger-icon mobile" onClick={toggleMenu}>
-        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
-      </div>
-
-      {/* Menu para telas menores */}
-      <div className={`navbar-menu mobile ${menuOpen ? "open" : ""}`}>
-        {techOptions.map((tech, index) => (
-          <label key={index} className="navbar-item">
-            <input
-              type="radio"
-              name="tech-stack"
-              value={tech}
-              checked={selectedTech === tech}
-              onChange={() => {
-                onTechChange(tech);
-                toggleMenu(); // Fechar o menu após seleção
-              }}
-            />
-            {tech}
+            <span className="name">{tech}</span>
           </label>
         ))}
       </div>
