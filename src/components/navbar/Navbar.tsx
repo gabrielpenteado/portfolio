60dd6eb47a0a8670c03403bbb58ac305a95fa75f
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-09 16:54:00 -0300

    updated navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface NavbarProps {
   selectedTech: string;
@@ -12,20 +14,53 @@ const Navbar: React.FC<NavbarProps> = ({
   onTechChange,
   techOptions,
 }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      {techOptions.map((tech, index) => (
-        <label key={index} className="navbar-item">
-          <input
-            type="radio"
-            name="tech-stack"
-            value={tech}
-            checked={selectedTech === tech}
-            onChange={() => onTechChange(tech)}
-          />
-          {tech}
-        </label>
-      ))}
+      {/* Navbar tradicional para telas maiores */}
+      <div className="navbar-menu desktop">
+        {techOptions.map((tech, index) => (
+          <label key={index} className="navbar-item">
+            <input
+              type="radio"
+              name="tech-stack"
+              value={tech}
+              checked={selectedTech === tech}
+              onChange={() => onTechChange(tech)}
+            />
+            {tech}
+          </label>
+        ))}
+      </div>
+
+      {/* Ícone de Hambúrguer para telas menores */}
+      <div className="hamburger-icon mobile" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+      </div>
+
+      {/* Menu para telas menores */}
+      <div className={`navbar-menu mobile ${menuOpen ? "open" : ""}`}>
+        {techOptions.map((tech, index) => (
+          <label key={index} className="navbar-item">
+            <input
+              type="radio"
+              name="tech-stack"
+              value={tech}
+              checked={selectedTech === tech}
+              onChange={() => {
+                onTechChange(tech);
+                toggleMenu(); // Fechar o menu após seleção
+              }}
+            />
+            {tech}
+          </label>
+        ))}
+      </div>
     </nav>
   );
 };
