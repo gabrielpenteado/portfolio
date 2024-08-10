36d1e4cf9d2588f57b1009a5903d40e157085a23
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-10 13:07:25 -0300

    updated navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
 import "./Navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface NavbarProps {
   selectedTech: string;
@@ -14,18 +11,11 @@ const Navbar: React.FC<NavbarProps> = ({
   onTechChange,
   techOptions,
 }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <nav className="navbar">
-      {/* Navbar tradicional para telas maiores */}
-      <div className="navbar-menu desktop">
+      <div className="navbar-menu desktop radio-inputs">
         {techOptions.map((tech, index) => (
-          <label key={index} className="navbar-item">
+          <label key={index} className="navbar-item radio">
             <input
               type="radio"
               name="tech-stack"
@@ -33,31 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({
               checked={selectedTech === tech}
               onChange={() => onTechChange(tech)}
             />
-            {tech}
-          </label>
-        ))}
-      </div>
-
-      {/* Ícone de Hambúrguer para telas menores */}
-      <div className="hamburger-icon mobile" onClick={toggleMenu}>
-        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
-      </div>
-
-      {/* Menu para telas menores */}
-      <div className={`navbar-menu mobile ${menuOpen ? "open" : ""}`}>
-        {techOptions.map((tech, index) => (
-          <label key={index} className="navbar-item">
-            <input
-              type="radio"
-              name="tech-stack"
-              value={tech}
-              checked={selectedTech === tech}
-              onChange={() => {
-                onTechChange(tech);
-                toggleMenu(); // Fechar o menu após seleção
-              }}
-            />
-            {tech}
+            <span className="name">{tech}</span>
           </label>
         ))}
       </div>
