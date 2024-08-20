65cb0a466afbf5912faa5cccb7e556fd13129d95
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-20 20:44:34 -0300

    updated viteconfig base and react router to /portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/portfolio",
     element: <App />,
     //   errorElement: <ErrorPage />,
   },
