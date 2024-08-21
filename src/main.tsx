efe6d7045e329f55d66b6deee22f670e0db2935c
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-20 21:27:42 -0300

    new config

--- src/main.tsx
+++ src/main.tsx
@@ -2,30 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-import {
-  createBrowserRouter,
-  HashRouter,
-  RouterProvider,
-  Routes,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/portfolio",
-      element: <App />,
-      //   errorElement: <ErrorPage />,
-    },
-  ],
-  { basename: import.meta.env.DEV ? "/" : "/portfolio" }
-);
+const router = createBrowserRouter([
+  {
+    path: "/portfolio",
+    element: <App />,
+    //   errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <RouterProvider router={router} />
-      </Routes>
-    </HashRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/portfolio";
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: "/portfolio/",
 });
